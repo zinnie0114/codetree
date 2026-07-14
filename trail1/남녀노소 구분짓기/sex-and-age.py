06dcc0cf010e08eff60a5e498d1ee4a2fd745c05
zinnie0114 <jdmsdpfxm@hufs.ac.kr>
2026-07-14 23:53:59 +0900

    [Passed] 남녀노소 구분짓기 (45ms, 16MB)

---
+++ trail1/남녀노소 구분짓기/sex-and-age.py
@@ -0,0 +1,14 @@
+gender= int(input())
+age=int(input())
+
+if gender == 0: 
+    if age>=19:
+        print("MAN")
+    else:
+        print("BOY")
+else: 
+    if age >= 19:
+        print("WOMAN")
+    else:
+        print('GIRL')
+    
